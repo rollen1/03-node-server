4c544449070e9dc3bd5959dce928dbe2337bcb7f
rollen1 <nolovedivision@gmail.com>
2022-06-10 20:59:04 +0300

    refactoring

---
+++ README.md
@@ -0,0 +1,25 @@
+# Nodejs Server
+
+_38 group project_
+
+## Project features
+
+-   Node.js: http
+-   Static file serving
+
+## Running the server
+
+In `dev` mode:
+
+```
+node ./index.js
+NODE_ENV=dev node ./index.js
+NODE_ENV=test node ./index.js
+NODE_ENV=pomidoras node ./index.js
+```
+
+In a specific (pre-defined) `prod` mode:
+
+```
+NODE_ENV=prod node ./index.js
+```
--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const config = {};
 
-config.dev = { 
+config.dev = {
+    name: 'dev',
+    httpPort: 6969,
     passwordLength: 2,
     defaultLanguage: 'en',
+    languages: ['en', 'lt', 'ee'],
     db: {
         user: 'root',
         password: 'admin',
@@ -11,11 +14,14 @@ config.dev = {
 }
 
 config.prod = {
+    name: 'prod',
+    httpPort: 42069,
     passwordLength: 12,
     defaultLanguage: 'lt',
+    languages: ['en', 'lt'],
     db: {
         user: 'node_batai_user',
-        password: '636363263626326326',
+        password: 'r84tr5s25e84rrg52f5er84r5ert8r4g55e',
         database: 'batai-r5fe1d15',
     },
 }
@@ -25,6 +31,7 @@ const env = nodeEnv ? nodeEnv : 'dev';
 const options = config[env] ? config[env] : config.dev;
 
 // console.log('kur dirba kodas?');
-// console.log('ka parasiau terminale: ', nodeEnv);
-// console.log('kokia aplinka turesiu paleisti:', env);
-console.log(options);
+// console.log('Ka parasiau terminale:', nodeEnv);
+// console.log('Kokia aplinka turesiu paleisti:', env);
+
+export default options;
