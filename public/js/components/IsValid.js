2ef228efc6de0e39690fac722155615b0a078eac
rollen1 <nolovedivision@gmail.com>
2022-06-22 18:05:56 +0300

    jest

--- public/js/components/IsValid.js
+++ public/js/components/IsValid.js
@@ -1,12 +1,49 @@
 class IsValid {
     static fullname(str) {
-        if (str.length < 2) {
-            return [true, 'Per trumpas fullname tekstas'];
+        if (str === undefined) {
+            return [true, 'Neduotas parametras'];
+        }
+        if (typeof str !== 'string') {
+            return [true, 'Netinkamas tipas, turi buti "string"'];
         }
 
-        const parts = str.split(' ');
-        if (parts.length < 2) {
-            return [true, 'Per mazai fullname zodziu'];
+        str = str.trim().replace(/\s+/g, ' ');
+        // str = str.trim().replaceAll('  ', ' '); // nes pas mane sitas neveikia :(
+
+        const minWordsCount = 2;
+        const minWordLength = 2;
+        const minTextLength = minWordsCount * minWordLength + (minWordsCount - 1);
+        if (str.length < minTextLength) {
+            return [true, `Per trumpas tekstas, turi buti minimum ${minTextLength} simboliai`];
+        }
+
+        const allowedSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const words = str.split(' ');
+        if (words.length < minWordsCount) {
+            return [true, `Tekstas turi tureti ${minWordsCount} arba daigiau zodziu`];
+        }
+        for (const word of words) {
+            if (word.length < minWordLength) {
+                return [true, `Visos vardo dalys turi buti minimum ${minWordLength} simboliu`];
+            }
+
+            // pirma raide
+            if (word[0].toUpperCase() !== word[0]) {
+                return [true, `Pirma zodzio raide turi buti didzioji`];
+            }
+
+            // kitos raides
+            const otherLetters = word.slice(1);
+            if (otherLetters.toLowerCase() !== otherLetters) {
+                return [true, `Ne pirma zodzio raide turi buti mazoji`];
+            }
+
+            // ar tik leistinos raides
+            for (const s of word) {
+                if (!allowedSymbols.includes(s)) {
+                    return [true, `Neleistinas simbolis "${s}"`];
+                }
+            }
         }
 
         return [false, 'OK'];
